6364064981e13b04e280a3154062470cbf25902c
deb-dasit <deb.dasit2013@gmail.com>
2020-03-29 04:35:59 +0530

    Models

--- COVID19/apisv1/models.py
+++ COVID19/apisv1/models.py
@@ -3,26 +3,81 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Category(models.Model):
-    name = models.CharField(max_length=255)
-    timestamp = models.DateTimeField(auto_now=True)
 
-
-class Stores(models.Model):
+class Shop(models.Model):
+    '''
+    Shop details model
+    '''
     name = models.CharField(max_length=255)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    state = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    locality = models.CharField(max_length=100)
+    address = models.TextField()
+    pin = models.CharField(max_length=6)
     timestamp = models.DateTimeField(auto_now=True)
+
+
+class VolunteerShops(models.Model):
+    '''
+    Map between Volunteer and shop(s)
+    '''
+    volunteer = models.ForeignKey(User, on_delete=models.CASCADE)
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
 
 
 class Items(models.Model):
-    name = models.CharField(max_length=255)
-    mrp = models.DecimalField(decimal_places=2)
-    batch_no = models.CharField(max_length=255)
-    manf_date = models.DateField()
-    exp_date = models.DateField()
-    store_id = models.ForeignKey(Stores, on_delete=models.CASCADE)
+    '''
+    Item list
+    '''
+    item = models.CharField(max_length=255)
+    quantity = models.CharField(max_length=5)
+
+
+class UserOrder(models.Model):
+    '''
+    Map between User and Item, along with the status of the order from user and shopkeeper respectively
+    Order_status: 0: hold
+                  1: confirm
+                  2: reject
+    '''
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    store = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    item_list = models.ForeignKey(Items, on_delete=models.CASCADE)
+    order_status = models.IntegerField(default=0)
+    acceptance_type = models.CharField(max_length=25)
+    order_item_status = models.BooleanField(default=0)
+    user_finalised = models.BooleanField(default=0)
     timestamp = models.DateTimeField(auto_now=True)
-    stock = models.CharField(max_length=255)
+
+
+class ItemComments(models.Model):
+    '''
+    Comments on any item of an order
+    '''
+    item = models.ForeignKey(Items, on_delete=models.CASCADE)
+    comments = models.TextField()
+    comment_for = models.CharField(max_length=12)
+    status = models.BooleanField(default=0)
+    timestamp = models.DateTimeField(auto_now=True)
+
+
+class VolunteerOrder(models.Model):
+    '''
+    Map between Volunteer and Order
+    '''
+    volunteer = models.ForeignKey(User, on_delete=models.CASCADE)
+    order = models.ForeignKey(UserOrder, on_delete=models.CASCADE)
+
+
+class Feedback(models.Model):
+    '''
+    User's feedback on the order
+    '''
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(default=0)
+    order = models.ForeignKey(UserOrder, on_delete=models.CASCADE)
+    feedback = models.TextField()
+    timestamp = models.DateTimeField(auto_now=True)
 
 
